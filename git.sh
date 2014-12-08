df165b33ac1c7bbcefb1b0fdaaa09793bf745861
horald <horst.meyer.koeln@gmail.com>
2014-12-08 22:31:47 +0100

    Changes of Vers 1.00.014

---
+++ git.sh
@@ -0,0 +1,11 @@
+git add --all
+if [ "$1" != "" ]; then
+  echo "$1"
+  git commit -m "$1"
+else
+  echo "Changes of Vers 1.00.014"
+  git commit -m "Changes of Vers 1.00.014"
+fi
+git push meinspielplatz
+
+
